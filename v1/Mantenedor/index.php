f92bdd02e2062a62eadf85d23698d88df45d002a
IgnaT-M <wpablotm@gmail.com>
2024-04-19 23:07:12 -0400

    ?

--- v1/Mantenedor/index.php
+++ v1/Mantenedor/index.php
@@ -31,12 +31,34 @@ if ($_version == 'V1'){
                         
                         ],
                         [
-                            "id" => "1",
+                            "id" => "2",
                             "titulo" => [
-                                "esp" => "Consultoría digital"
+                                "esp" => "Soluciones multiexperiencia"
                             ],
                             "texto" => [
-                                "esp" => "Identificamos las fallas y conectamos los puntos entre tu negocio y tu estrategia digital. Nuestro equipo experto cuenta con años de experiencia en la definición de estrategias y hojas de ruta en función de tus objetivos específicos."
+                                "esp" => "Deleitamos a las personas usuarias con experiencias interconectadas a través de aplicaciones web, móviles, interfaces conversacionales, digital twin, IoT y AR. Su arquitectura puede adaptarse y evolucionar para adaptarse a los cambios de tu organización."
+                            ],
+                            "activo" => true
+                        
+                        ],
+                        [
+                            "id" => "3",
+                            "titulo" => [
+                                "esp" => "Evolución de ecosistemas"
+                            ],
+                            "texto" => [
+                                "esp" => "Ayudamos a las empresas a evolucionar y ejecutar sus aplicaciones de forma eficiente, desplegando equipos especializados en la modernización y el mantenimiento de ecosistemas técnicos. Creando soluciones robustas en tecnologías de vanguardia."
+                            ],
+                            "activo" => true
+                        
+                        ],
+                        [
+                            "id" => "4",
+                            "titulo" => [
+                                "esp" => "Soluciones Low-Code"
+                            ],
+                            "texto" => [
+                                "esp" => "Traemos el poder de las soluciones low-code y no-code para ayudar a nuestros clientes a acelerar su salida al mercado y añadir valor. Aumentamos la productividad y la calidad, reduciendo los requisitos de cualificación de los desarrolladores."
                             ],
                             "activo" => true
                         
